145376b236695f306bbee30673b5888b064d21c8
Gustavo Gabriel <gugagabriel56@gmail.com>
2025-01-21 19:45:54 -0300

    Início do projeto Spring Boot

---
+++ src/test/java/com/ugustavob/springjwtauthentication/SpringJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugustavob.springjwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
